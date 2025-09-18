df9df08c38924563237d82c3e66f8e3ab00c1036
hanna <nurhanalut9878@gmail.com>
2025-09-18 15:18:07 +0800

    test4

--- dasar.php
+++ dasar.php
@@ -20,32 +20,6 @@
             <li><a href="visi.php">VISI & MISI</a></li>
             <li><a href="moto.php">MOTO EKSA</a></li>
             <li><a href="zon.php">INFO ZON</a></li>
-            <li class="dropdown">
-                    <a href="zon.php">INFO ZON <span class="dropdown-arrow">▼</span></a>
-                    <div class="dropdown-content">
-                        <a href="zon.php?zone=bougainvillea">BOUGAINVILLEA</a>
-                        <a href="zon.php?zone=fiddle-fig">FIDDLE FIG</a>
-                        <a href="zon.php?zone=monstera">MONSTERA</a>
-                        <a href="zon.php?zone=peace-lily">PEACE LILY</a>
-                        <a href="zon.php?zone=bamboo">BAMBOO</a>
-                        <a href="zon.php?zone=jasmine">JASMINE</a>
-                        <a href="zon.php?zone=philodendron">PHILODENDRON</a>
-                        <a href="zon.php?zone=spider-plant">SPIDER PLANT</a>
-                        <a href="zon.php?zone=lavender">LAVENDER</a>
-                        <a href="zon.php?zone=aloe-vera">ALOE VERA</a>
-                        <a href="zon.php?zone=golden-palm">GOLDEN PALM</a>
-                        <a href="zon.php?zone=chrysanthemum">CHRYSANTHEMUM</a>
-                        <a href="zon.php?zone=dracaena">DRACAENA</a>
-                        <a href="zon.php?zone=golden-pothos">GOLDEN POTHOS</a>
-                        <a href="zon.php?zone=fern">FERN</a>
-                        <a href="zon.php?zone=daisy">DAISY</a>
-                        <a href="zon.php?zone=cactus">CACTUS</a>
-                        <a href="zon.php?zone=calathea">CALATHEA</a>
-                        <a href="zon.php?zone=ixora">IXORA</a>
-                        <a href="zon.php?zone=snake-plant">SNAKE PLANT</a>
-                        <a href="zon.php?zone=rhapis-palm">RHAPIS PALM</a>
-                    </div>
-                </li>
         </ul>
       
     </nav>
